9b8c32d96be9f9db3499d59d59be071d220bfdc2
firatkaya1 <firat.kaya@mail.com>
2019-08-22 19:34:15 +0300

    created and completed. This is a calculator and created by Apple Company, İ changed for android users, so you can use easily for your android mobile phone.

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -22,6 +22,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         init();
 
         btnAdd.setOnClickListener(new View.OnClickListener() {
